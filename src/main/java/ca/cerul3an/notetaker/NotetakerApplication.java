6852e1f2c2f504ac8e6b68a8fabbbf76bbb1ce30
Tong Su <99ttss@gmail.com>
2018-03-11 19:32:30 -0400

    Initial Commit

---
+++ src/main/java/ca/cerul3an/notetaker/NotetakerApplication.java
@@ -0,0 +1,12 @@
+package ca.cerul3an.notetaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotetakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotetakerApplication.class, args);
+	}
+}
