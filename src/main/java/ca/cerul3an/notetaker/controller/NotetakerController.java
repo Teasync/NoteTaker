60339e9e9bcaa0b190c403091558b8c2160e40bb
Teasync <99ttss@gmail.com>
2018-03-12 23:28:50 -0400

    Implemented home, create, and error pages

--- src/main/java/ca/cerul3an/notetaker/controller/NotetakerController.java
+++ src/main/java/ca/cerul3an/notetaker/controller/NotetakerController.java
@@ -4,6 +4,7 @@ import ca.cerul3an.notetaker.exception.ResourceNotFoundException;
 import ca.cerul3an.notetaker.model.Note;
 import ca.cerul3an.notetaker.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -16,9 +17,14 @@ public class NotetakerController {
     @Autowired
     NoteRepository noteRepository;
 
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleResourceNotFoundException() {
+        return "notfound";
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("active", "active");
         return "index";
     }
 
@@ -36,7 +42,13 @@ public class NotetakerController {
 
     @GetMapping("/create")
     public String create(Model model) {
+        model.addAttribute("note", new Note());
         return "create";
     }
 
+    @PostMapping("/create")
+    public String createNote(@Valid @ModelAttribute Note note) {
+        noteRepository.save(note);
+        return "createDone";
+    }
 }
